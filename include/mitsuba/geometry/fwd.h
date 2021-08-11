e08cf139dab3bcfe4c6364d4b204a03d9435624f
hyeonjang <ippguu@gmail.com>
2021-08-11 21:43:02 +0900

    add declaration of halfedge mesh features

--- include/mitsuba/geometry/fwd.h
+++ include/mitsuba/geometry/fwd.h
@@ -7,8 +7,15 @@
 
 NAMESPACE_BEGIN(mitsuba)
 
-struct Chunk;
+NAMESPACE_BEGIN(geometry)
+template <typename Float> struct Element;
+template <typename Float> struct Vertex;
+template <typename Float> struct Halfedge;
+template <typename Float> struct Edge;
+
+template <typename Float, typename Spectrum> class PolygonMesh;
 template <typename Float, typename Spectrum> class SurfaceMesh;
+NAMESPACE_END(geometry)
 
 template <typename Float_, typename Spectrum_> struct GeometryAliases {
     using Float                  = Float_;
@@ -18,26 +25,31 @@ template <typename Float_, typename Spectrum_> struct GeometryAliases {
     using FloatU                 = underlying_t<Float>;
     using SpectrumU              = underlying_t<Spectrum>;
 
-    using Chunk                  = mitsuba::Chunk;
+    using Vertex                 = mitsuba::geometry::Vertex<FloatU>;
+    using Halfedge               = mitsuba::geometry::Halfedge<FloatU>;
+    using Edge                   = mitsuba::geometry::Edge<FloatU>;
 
-    using SurfaceMesh            = mitsuba::SurfaceMesh<FloatU, SpectrumU>;
+    using PolygonMesh            = mitsuba::geometry::PolygonMesh<FloatU, SpectrumU>;
+    using SurfaceMesh            = mitsuba::geometry::SurfaceMesh<FloatU, SpectrumU>;
 };
 
-#define MTS_GEOMETRY_IMPORT_BASIC_TYPES()                                                \
-    MTS_IMPORT_CORE_TYPES()                                                              \
-    using GeometryAliases      = mitsuba::GeometryAliases<Float, Spectrum>;              
+#define MTS_GEOMETRY_IMPORT_BASIC_TYPES()                                       \
+    MTS_IMPORT_CORE_TYPES()                                                     \
+    using GeometryAliases      = mitsuba::GeometryAliases<Float, Spectrum>;     \
+    using Vertex               = typename GeometryAliases::Vertex;              \
+    using Halfedge             = typename GeometryAliases::Halfedge;            \
+    using Edge                 = typename GeometryAliases::Edge;
 
 // This macro is needed to get this to compile across all compilers
 #define MTS_GEOMETRY_IMPORT_TYPES_HELPER(...) GeometryAliases, ##__VA_ARGS__
 
 #define MTS_GEOMETRY_IMPORT_TYPES(...)                                                   \
     MTS_GEOMETRY_IMPORT_BASIC_TYPES()                                                    \
-    using Chunk = typename GeometryAliases::Chunk;                                       \
     ENOKI_USING_TYPES(MTS_GEOMETRY_IMPORT_TYPES_HELPER(__VA_ARGS__))
 
-#define MTS_GEOMETRY_IMPORT_OBJECT_TYPES()                                               \
-    using SurfaceMesh            = typename GeometryAliases::SurfaceMesh;                
-
-// -----------------------------------------------------------------------------
+#define MTS_GEOMETRY_IMPORT_OBJECT_TYPES()                                \
+    using PolygonMesh            = typename GeometryAliases::PolygonMesh; \
+    using SurfaceMesh            = typename GeometryAliases::SurfaceMesh;
+    // -----------------------------------------------------------------------------
 
 NAMESPACE_END(mitsuba)
