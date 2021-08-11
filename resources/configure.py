e8b3814743cc3229ec61635d4561bee70fa6e93d
hyeonjang <ippguu@gmail.com>
2021-08-11 14:28:56 +0900

    add geometry configuration

--- resources/configure.py
+++ resources/configure.py
@@ -64,6 +64,19 @@ def write_core_config_cpp(f, enabled, default_variant):
         w('    MTS_EXTERN_RENDER template class MTS_EXPORT_RENDER Name<%s, %s>;' % (float_, spectrum))
     f.write('\n')
 
+    f.write('/// Declare that a "struct" template is to be imported and not instantiated\n')
+    w('#define MTS_EXTERN_STRUCT_GEOMETRY(Name)')
+    for index, (name, float_, spectrum) in enumerate(enabled):
+        w('    MTS_EXTERN_RENDER template struct MTS_EXPORT_RENDER Name<%s, %s>;' % (float_, spectrum))
+    f.write('\n')
+
+    f.write('/// Declare that a "class" template is to be imported and not instantiated\n')
+    w('#define MTS_EXTERN_CLASS_GEOMETRY(Name)')
+    for index, (name, float_, spectrum) in enumerate(enabled):
+        w('    MTS_EXTERN_RENDER template class MTS_EXPORT_RENDER Name<%s, %s>;' % (float_, spectrum))
+    f.write('\n')
+
+
     f.write('/// Explicitly instantiate all variants of a "struct" template\n')
     w('#define MTS_INSTANTIATE_STRUCT(Name)')
     for index, (name, float_, spectrum) in enumerate(enabled):
